beee593d400360e20a6d81e218311896d12bb98c
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-03-02 12:42:29 +0300

    footer

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom"
 import "./Nav.styled"
+import { Div } from "./Nav.styled"
 
 const Nav = () => {
   return (
-    <div>
+    <Div>
         <Link to="/">Home</Link>
         <Link to="/people">People</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/contact">Contact</Link>
-    </div>
+    </Div>
   )
 }
 
--- src/components/nav/Nav.styled.jsx
+++ src/components/nav/Nav.styled.jsx
@@ -1,2 +1,9 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+background-color:green;
+
+`;
+
 
 
